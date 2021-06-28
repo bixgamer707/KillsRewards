529e67978db20a76181f5497165077b488dede87
bixgamer 707 <85091403+bixgamer707@users.noreply.github.com>
2021-06-28 11:52:08 -0500

    Delete KillsRewards directory

--- KillsRewards/src/bixgamer707/kr/utils/Config.java
+++
@@ -1,8 +0,0 @@
-package bixgamer707.kr.utils;
-
-public class Config extends Files {
-
-    public Config(){
-        super("config.yml");
-    }
-}
--- KillsRewards/src/bixgamer707/kr/utils/Messages.java
+++
@@ -1,8 +0,0 @@
-package bixgamer707.kr.utils;
-
-public class Messages extends Files {
-
-    public Messages(){
-        super("messages.yml");
-    }
-}
